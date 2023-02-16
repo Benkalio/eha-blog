547889a27145657fc6adc6b3de920a8b48d18403
Benkalio <officialtennysonbk@gmail.com>
2023-02-16 14:56:39 +0100

    setup restructure

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to EHA Blog</h1>
+      <p>Content</p>
+    </div>
+  )
+}
